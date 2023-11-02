227929a8f1ec510c2c661de2f69a296312bd3bbe
Eman288 <emantamam107@gmail.com>
2023-11-02 16:22:53 -0700

    0x1D-task19-betty

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,7 +8,8 @@
  * Return: the Lowest common ancestor or NULL
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	if (first == NULL || second == NULL)
 		return (NULL);
