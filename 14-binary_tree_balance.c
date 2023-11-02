220f2be50f2db1b84b5fab6a49d4ee48148d5d69
root <besckinder@gmail.com>
2023-11-02 05:22:21 -0700

    Betty fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height = binary_tree_height(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
-} 
+}
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
