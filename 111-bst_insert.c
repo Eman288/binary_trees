5f4e08957567855c80a27f06f7490740cf733958
Eman288 <emantamam107@gmail.com>
2023-11-02 15:41:31 -0700

    0x1D-task25

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -9,5 +9,42 @@
 
 bst_t *bst_insert(bst_t **tree, int value)
 {
+	bst_t *root, *node;
 
+	root = *tree;
+	if (tree == NULL)
+		return (NULL);
+	if (root == NULL)
+	{
+		node = binary_tree_node(root, value);
+		if (!node)
+			return (NULL);
+		*tree = node;
+		return (node);
+	}
+	if (value < root->n)
+	{
+		if (root->left != NULL)
+		{
+			return (bst_insert(&root->left, value));
+		}
+		node = binary_tree_node(root, value);
+                if (!node)
+                        return (NULL);
+                root->left = node;
+                return (node);
+	}
+	else if (value > root->n)
+	{
+		if (root->right != NULL)
+		{
+			return (bst_insert(&root->right, value));
+		}
+		node = binary_tree_node(root, value);
+                if (!node)
+                        return (NULL);
+                root->right = node;
+                return (node);
+        }
+	return (NULL);
 }
