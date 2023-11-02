adbf8004daaa4e335a34b3dd6a14e4ce5c350bff
Eman288 <emantamam107@gmail.com>
2023-11-02 06:51:36 -0700

    0x1D-task15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,37 +1,33 @@
 #include "binary_trees.h"
 
 /**
+ * check - a function that checks for the fullness of a node
+ * @tree: the nide
+ * Return: 1 if it is full, 0 is not
+ */
+
+int check(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left == NULL && tree->right != NULL) ||
+				(tree->left != NULL && tree->right == NULL)
+				|| (check(tree->left) == 0) ||
+				(check(tree->right) == 0))
+			return (0);
+	}
+	return (1);
+}
+/**
  * binary_tree_is_full - a function that checks if a binary tree is full
  * @tree: the root node
  * Return: 1 if it is full, 0 is not
  */
-
-int check(const binary_tree_t *tree, int i)
-{
-	int j;
-
-	if (tree != NULL)
-	{
-		if (tree->left && tree->right && tree->parent != NULL)
-			j = 0;
-		else
-			j = 1;
-		i += j;
-		check(tree->left, i);
-		check(tree->right, i);
-	}
-	return (i);
-}
-
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int i = 0;
-	if(tree != NULL)
+	if(tree ==  NULL)
 	{
-		if (check(tree, i) == 0)
-			return (1);
-		else
-			return (0);
+		return (0);
 	}
-	return (1);
+	return (check(tree));
 }
