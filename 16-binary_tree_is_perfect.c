9a18591ed74952fb141d184914d8996df67f7096
root <besckinder@gmail.com>
2023-11-02 05:45:59 -0700

    checker fix attempt 1

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -60,8 +60,8 @@ int  binary_tree_is_perfect(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height == right_height)
-		return (1);
+	if (left_height != right_height)
+		return (0);
 
-	return (0);
+	return (1);
 }
