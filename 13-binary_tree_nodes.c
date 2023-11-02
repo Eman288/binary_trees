7bfc794d693dbb5aef606b307f8da33f485c836a
Eman288 <emantamam107@gmail.com>
2023-11-01 17:38:21 -0700

    0x1D-task13/betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - a function that counts the nodes with at least 1 child 
- * in a binary tree
- * @tree: the node
- * Return: number of nodes
- */
+* binary_tree_nodes - a function that counts the nodes with at least 1 child
+* in a binary tree
+* @tree: the node
+* Return: number of nodes
+*/
 
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t i;
 
 	i = 0;
-	
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left || tree->right)
