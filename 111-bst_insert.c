c2650f068e5b76c81a3a04f23e657c13d7b9c884
Eman288 <emantamam107@gmail.com>
2023-11-02 15:08:40 -0700

    binary_trees fix

---
+++ 111-bst_insert.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - a function that inserts a value in a Binary Search Tree
+ * @tree: the root
+ * @value: the value to be insearted
+ * Return: a pointer to the created node,
+ */
+
+bst_t *bst_insert(bst_t **tree, int value)
+{
+
+}
