e2d831dc37f9a357bd3625bb08de79a4ff713b12
root <besckinder@gmail.com>
2023-11-02 19:36:49 -0700

    Betty fix

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -11,10 +11,10 @@ int is_bst_helper(const binary_tree_t *tree, int min, int max);
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (1);
+	if (tree == NULL)
+		return (1);
 
-        return (is_bst_helper(tree, -10000, 10000));
+	return (is_bst_helper(tree, -10000, 10000));
 }
 
 /**
@@ -28,13 +28,13 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 
 int is_bst_helper(const binary_tree_t *tree, int min, int max)
 {
-        if (tree == NULL)
-                return (1);
-        if (tree->n < min || tree->n > max)
-                return (0);
+	if (tree == NULL)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
 
-        return (is_bst_helper(tree->left, min, tree->n - 1) &&
-                        is_bst_helper(tree->right, tree->n + 1, max));
+	return (is_bst_helper(tree->left, min, tree->n - 1) &&
+			is_bst_helper(tree->right, tree->n + 1, max));
 }
 
 
