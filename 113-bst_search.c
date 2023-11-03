28424ec67fa7e0482e73c83c2b200db0a5a7dd7b
Eman288 <emantamam107@gmail.com>
2023-11-02 17:00:51 -0700

    header file change

---
+++ 113-bst_search.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * bst_search - a function that searches for a value in a Binary Search Tree
+ * @tree: the root
+ * @value:
+ * the value to search for
+ * Return: the node
+ */
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else if (value > tree->n)
+		return (bst_search(tree->right, value));
+	else
+		return (tree);
+}
